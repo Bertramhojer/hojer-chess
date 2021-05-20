48d1b19c5329e9b59ba1b7af2cedb7a49d787b1c
Bertram <ber.hoejer@gmail.com>
2021-05-20 16:27:10 +0200

    updated evaluation-function and documentation

--- evaluation.py
+++ evaluation.py
@@ -1,13 +1,17 @@
-""""
-Will contain multiple different move-evaluation algorithms 
-"""
 import chess
-import constants
+import constants_sun
 import random
-import game
 
 
 class BaseEngine:
+    """
+    Base class for evaluation classes containing opening knowledge.
+
+    - functions -
+    is_opening() : checks the state of the game
+    get_opening_move() : finds opening move if in database
+    make_opening_move() : returns an opening move as chess.Move object
+    """
     
     def __init__(self, board, made_moves, user):
         self.board = board
@@ -18,18 +22,29 @@ class BaseEngine:
         """
         Check if the game is in the opening phase
         """
+
         if self.board.fullmove_number <= 5:
             return True
         return False
 
     def get_opening_move(self):
+        """
+        Gets the correct theoretical response to certain opening moves.
+
+        - functions -
+        find_move() : checks the response to a move from a given opening
+
+        - variables -
+        sicilian (list) : opening moves for the open sicilian
+        q_gambit (list) : opening moves for the queens gambit declined 
+        """
 
         # Engine playing black
         sicilian = [['e2e4', 'g1f3', 'd2d4', 'f3d4'], ['c7c5', 'd7d6', 'c5d4', 'g8f6']]
         q_gambit = [['d2d4', 'c2c4', 'b1c3', 'g1f3'], ['d7d5', 'e7e6', 'g8f6', 'c7c6']]
-        caro_kann = [[], []]
 
         # Engine playing white
+        # add openings
 
         # Function to run through opening-lines
         def find_move(self, opening_moves):
@@ -78,9 +93,9 @@ class BaseEngine:
 class RandomEval(BaseEngine):
     """
     Engine class that does no board evalatuon but produces a random move.
+    Inherits BaseEngine-Class.
 
     Functions:
-    Init intitiates the class with the current board from game.py
     make_move(): loops through legal moves and returns a random legal move
     """
 
@@ -104,31 +119,36 @@ class SimpleEval(BaseEngine):
     """
     Engine Class performing a simple evaluation inspired by Michniewski's simple evaluation;
     https://www.chessprogramming.org/Simplified_Evaluation_Function
+    Inherits BaseEngine-Class.
 
-
-    Functions:
-    is_end_game() : takes no arguments and determines end-game status by simple piece evaluation
-    evaluate_piece() : gets the current value of a given piece estimated from its intrinsic value added by the piece-square table value.
+    - functions -
+    is_end_game() : determines end-game status by simple piece evaluation
+    evaluate_piece() : evaluates piece by piece-square tables
     is_capture() : checks whether a move is a capture
     evaluate_capture() : evaluates whether a trade is favorable
+    under_attack_now() : checks for attacks at current square
+    under_attack_future() : checks for attacks in future square
     is_attacked() : checks whether a given move will place the piece under attack
     is_defended() : check whether a given move will place the piece on a defended square
     evaluate_board() : makes an evaluation of the board by calculating the values from evaluate_piece() for all pieces on the board.
     evaluate_move() : makes use of above-stated evaluation functions to find the best move in the current position.
-
     """
-
 
     def __init__(self, board, made_moves, user):
         super().__init__(board, made_moves, user)
     
 
-    def is_end_game(self, board):
+    def is_end_game(self):
+        """
+        Checks the state of the game as specified here;
+        https://www.chessprogramming.org/Simplified_Evaluation_Function
+        """
+
         queens = 0
         minors = 0
 
         for square in chess.SQUARES:
-            piece = board.piece_at(square)
+            piece = self.board.piece_at(square)
             if not piece:
                 continue
             if piece.piece_type == chess.QUEEN:
@@ -140,92 +160,135 @@ class SimpleEval(BaseEngine):
             return True
 
 
-    def evaluate_piece(self, board, piece, loc):
+    def evaluate_piece(self, piece, loc):
+        """
+
+        """
 
         type = piece.piece_type
         
         if piece.color == chess.WHITE:
             if type == chess.PAWN:
-                return constants.piece_value[1] + constants.white_pawn[loc]
+                return constants_sun.piece_value[1] + constants_sun.white_pawn[loc]
             if type == chess.KNIGHT:
-                return constants.piece_value[2] + constants.knight[loc]
+                return constants_sun.piece_value[2] + constants_sun.white_knight[loc]
             if type == chess.BISHOP:
-                return constants.piece_value[3] + constants.white_bishop[loc]
+                return constants_sun.piece_value[3] + constants_sun.white_bishop[loc]
             if type == chess.ROOK:
-                return constants.piece_value[4] + constants.white_rook[loc]
+                return constants_sun.piece_value[4] + constants_sun.white_rook[loc]
             if type == chess.QUEEN:
-                return constants.piece_value[5] + constants.queen[loc]
+                return constants_sun.piece_value[5] + constants_sun.white_queen[loc]
             if type == chess.KING:
-                if self.is_end_game(board):
-                    return constants.piece_value[6] + constants.white_king_end[loc]
+                if self.is_end_game():
+                    return constants_sun.piece_value[6] + constants_sun.white_king_end[loc]
                 else:
-                    return constants.piece_value[6] + constants.white_king_mid[loc]
+                    return constants_sun.piece_value[6] + constants_sun.white_king_mid[loc]
 
         elif piece.color == chess.BLACK:
             if type == chess.PAWN:
-                return constants.piece_value[1] + constants.black_pawn[loc]
+                return constants_sun.piece_value[1] + constants_sun.black_pawn[loc]
             if type == chess.KNIGHT:
-                return constants.piece_value[2] + constants.knight[loc]
+                return constants_sun.piece_value[2] + constants_sun.white_knight[loc]
             if type == chess.BISHOP:
-                return constants.piece_value[3] + constants.black_bishop[loc]
+                return constants_sun.piece_value[3] + constants_sun.black_bishop[loc]
             if type == chess.ROOK:
-                return constants.piece_value[4] + constants.black_rook[loc]
+                return constants_sun.piece_value[4] + constants_sun.black_rook[loc]
             if type == chess.QUEEN:
-                return constants.piece_value[5] + constants.queen[loc]
+                return constants_sun.piece_value[5] + constants_sun.white_queen[loc]
             if type == chess.KING:
-                if self.is_end_game(board):
-                    return constants.piece_value[6] + constants.black_king_end[loc]
+                if self.is_end_game():
+                    return constants_sun.piece_value[6] + constants_sun.black_king_end[loc]
                 else:
-                    return constants.piece_value[6] + constants.black_king_mid[loc]
+                    return constants_sun.piece_value[6] + constants_sun.black_king_mid[loc]
 
     
-    def is_capture(self, board, _sq):
-        _sq = board.piece_at(_sq)
+    def is_capture(self, _sq):
+        _sq = self.board.piece_at(_sq)
         if _sq is None:
             return False
         return True
     
 
-    def evaluate_capture(self, board, to_sq, from_sq):
+    def evaluate_capture(self, to_sq, from_sq):
 
-            piece_1 = board.piece_at(to_sq).piece_type
-            piece_2 = board.piece_at(from_sq).piece_type
+            # attacked piece
+            piece_1 = self.board.piece_at(to_sq).piece_type
+            # capturing piece
+            piece_2 = self.board.piece_at(from_sq).piece_type
 
-            piece_diff = (constants.piece_value[piece_1] - constants.piece_value[piece_2])
+            piece_diff = (constants_sun.piece_value[piece_1] - constants_sun.piece_value[piece_2])
 
             # e.g. knight for bishop or pawn for pawn
-            if piece_diff <= 0 and piece_diff > 100: 
+            if piece_diff >= 0 and piece_diff < 100: 
                 return 1
             # minor piece for major piece
-            if piece_diff >= 100 and piece_diff < 400:
+            if piece_diff >= 100 and piece_diff < 300:
                 return 2
             # minor/major piece for queen/king
-            if piece_diff >= 400:
+            if piece_diff >= 300:
                 return 3
             # badly weighed trade
             if piece_diff < 0:
                 return 0
+            
 
     
-    def under_attack(self, board, move):
+    def under_attack_now(self, move):
         # define a function that checks whether any opposite side pieces can attack the piece on current square
         if self.user == chess.WHITE:
-            if board.is_attacked_by(chess.WHITE, move.from_square):
-                return True
-            return False
+            if self.board.is_attacked_by(chess.WHITE, move.from_square):
+                attackers = list(self.board.attackers(chess.WHITE, move.from_square))
+                att_types = []
+                for att in attackers:
+                    att_types.append(self.board.piece_at(att).piece_type)
+                return att_types
+
         if self.user == chess.BLACK:
-            if board.is_attacked_by(chess.BLACK, move.from_square):
-                return True
-            return False
+            if self.board.is_attacked_by(chess.BLACK, move.from_square):
+                attackers = list(self.board.attackers(chess.BLACK, move.from_square))
+                att_types = []
+                for att in attackers:
+                    att_types.append(self.board.piece_at(att).piece_type)
+                return att_types
 
     
-    def to_sq_attacked(self, board, move):
+    def under_attack_future(self, move):
+        # define a function that checks whether any opposite side pieces can attack the piece on current square
         if self.user == chess.WHITE:
-            if board.is_attacked_by(chess.WHITE, move.to_square):
+            if self.board.is_attacked_by(chess.WHITE, move.to_square):
+                attackers = list(self.board.attackers(chess.WHITE, move.to_square))
+                att_types = []
+                for att in attackers:
+                    att_types.append(self.board.piece_at(att).piece_type)
+                return att_types
+
+        if self.user == chess.BLACK:
+            if self.board.is_attacked_by(chess.BLACK, move.to_square):
+                attackers = list(self.board.attackers(chess.BLACK, move.to_square))
+                att_types = []
+                for att in attackers:
+                    att_types.append(self.board.piece_at(att).piece_type)
+                return att_types
+
+    
+    def to_sq_attacked(self, move):
+        if self.user == chess.WHITE:
+            if self.board.is_attacked_by(chess.WHITE, move.to_square):
                 return True
             return False
         if self.user == chess.BLACK:
-            if board.is_attacked_by(chess.BLACK, move.to_square):
+            if self.board.is_attacked_by(chess.BLACK, move.to_square):
+                return True
+            return False
+    
+
+    def to_sq_defended(self, move):
+        if self.user == chess.WHITE:
+            if self.board.is_attacked_by(chess.BLACK, move.to_square):
+                return True
+            return False
+        if self.user == chess.BLACK:
+            if self.board.is_attacked_by(chess.WHITE, move.to_square):
                 return True
             return False
 
@@ -238,7 +301,7 @@ class SimpleEval(BaseEngine):
             if not piece:
                 continue
 
-            value = self.evaluate_piece(board, piece, sq)
+            value = self.evaluate_piece(piece, sq)
 
             if piece.color == chess.WHITE:
                 total += value
@@ -248,79 +311,100 @@ class SimpleEval(BaseEngine):
         return total
     
 
-    def evaluate_board_2(self, board, move):
+    def evaluate_board_2(self, move):
 
-        tmp_board = board.copy()
+        tmp_board = self.board.copy()
         tmp_board.push(move)
 
         return self.evaluate_board(tmp_board)
     
 
-    def move_is_check(self, board, move):
+    def move_is_check(self, move):
 
-        tmp_board = board.copy()
+        tmp_board = self.board.copy()
         tmp_board.push(move)
         if tmp_board.is_check():
             return True
         return False
             
 
-    def get_move_score(self, board, move):
+    def get_move_score(self, move):
 
         score = 0
         loc_1 = move.from_square
         loc_2 = move.to_square
-        piece = board.piece_at(loc_1)
+        piece = self.board.piece_at(loc_1)
+        cur_attackers = self.under_attack_now(move)
+        fut_attackers = self.under_attack_future(move)
 
-        if self.is_capture(board, loc_2):
-            capt_score = self.evaluate_capture(board, loc_1, loc_2)
+        if self.is_capture(loc_2):
+            capt_score = self.evaluate_capture(loc_2, loc_1)
             if capt_score == 0:
                 score -= 15
             elif capt_score == 1:
                 score += 15
             elif capt_score == 2:
-                score += 25
+                score += 30
             elif capt_score == 3:
-                score += 50
+                score += 100
         
-        if self.evaluate_piece(board, piece, loc_1) <= self.evaluate_piece(board, piece, loc_2):
-            score += (self.evaluate_piece(board, piece, loc_2) - self.evaluate_piece(board, piece, loc_1))
+        if self.evaluate_piece(piece, loc_1) <= self.evaluate_piece(piece, loc_2):
+            score += (self.evaluate_piece(piece, loc_2) - self.evaluate_piece(piece, loc_1))
         else:
-            score -= 5
+            score -= 10
         
-        if self.to_sq_attacked(board, move):
+        if self.to_sq_attacked(move):
             score -= 5
         else:
             score += 5
         
-        if self.move_is_check(board, move):
+        if self.to_sq_defended(move):
+            score += 10
+        
+        if self.move_is_check(move):
             score += 10
         
         if self.user == chess.WHITE:
-            if self.evaluate_board(board) < self.evaluate_board_2(board, move):
+            if self.evaluate_board(self.board) < self.evaluate_board_2(move):
                 score += 10
             else:
                 score -= 10
         else:
-            if self.evaluate_board(board) < self.evaluate_board_2(board, move):
+            if self.evaluate_board(self.board) < self.evaluate_board_2(move):
                 score -= 10
             else:
                 score += 10
         
-        if self.under_attack(board, move):
-            score += 40
+        if cur_attackers != None:
+            if (
+                piece.piece_type == 2 or piece.piece_type == 3 or 
+                piece.piece_type == 4 or piece.piece_type == 5
+                ):
+                if 1 in cur_attackers:
+                    score += 40
         
+        if fut_attackers != None:
+            if (
+                piece.piece_type == 2 or piece.piece_type == 3 or 
+                piece.piece_type == 4 or piece.piece_type == 5
+                ):
+                if 1 in fut_attackers:
+                    score -= 30
+
         return score
 
 
-    def get_best_move(self, board):
+    def get_best_move(self):
 
         all_moves = dict()
 
-        for move in board.legal_moves:
+        for move in self.board.legal_moves:
 
-            score = self.get_move_score(board, move)
+            score = self.get_move_score(move)
             all_moves[move] = score
+        
+        for i in list(all_moves.items()):
+            print(i)
         
         moves = dict(sorted(all_moves.items(), key=lambda x:x[1], reverse=True))
         best_move = list(moves.items())[0][0]
--- game.py
+++ game.py
@@ -2,10 +2,20 @@ import chess
 from constants import pieces
 import evaluation
 import matplotlib.pyplot as plt
-import time
 
 
 def play():
+    """
+    Runs the chess engine allowing for play with oneself or AI.
+
+    - variables -
+    board : instantiation of the chess.Board class from the python-chess library
+    game_mode : specifies the game-mode
+    engine : specifies the evaluation-algorithm to be used for playing
+    engine_move : the best move according to the evaluation algorithm
+    user_move : the move entered by the user
+    """
+
 
     # instantiate the board 
     board = chess.Board()
@@ -27,46 +37,59 @@ def play():
 
     elif game_mode == '2':
 
-        user = input("'w' to play as white or 'b' to play as black: ")
-        user = chess.WHITE if user == 'w' else chess.BLACK
+        while True:
+            user = input("'w' to play as white or 'b' to play as black: ")
+            if user == 'w':
+                user = chess.WHITE
+                break
+            elif user == 'b':
+                user = chess.BLACK
+                break
+            else:
+                print("Please enter either 'w' or 'b'")
 
-        engine = input("1) RandomEval   2) SimpleEval: ")
-
-        if engine == '1':
-            engine = evaluation.RandomEval(board, made_moves = list(), user=user)
-        if engine == '2':
-            engine = evaluation.SimpleEval(board, made_moves = list(), user=user)
-            evaluations = list()
+        while True:
+            engine = input("Enter 1 or 2: \n1) Random Move AI \n2) Simple Evaluation Algorithm AI\n")
+            if engine == '1':
+                engine = evaluation.RandomEval(board, made_moves = list(), user=user)
+                break
+            elif engine == '2':
+                engine = evaluation.SimpleEval(board, made_moves = list(), user=user)
+                evaluations = list()
+                break
 
         if user == chess.WHITE:
             print(display(board))
             user_move = make_move(board)
-            board.push(user_move)
+            engine.board.push(user_move)
             engine.made_moves.append(str(user_move))
-            evaluations.append(engine.evaluate_board(board))
+            evaluations.append(engine.evaluate_board(engine.board))
         
         while not board.is_game_over():
 
             # Check if we're in the opening
             if engine.is_opening():
                 engine_move = engine.make_opening_move()
+                # no opening move found
                 if engine_move == None:
-                    engine_move = engine.get_best_move(board)
+                    engine_move = engine.get_best_move()
             else:
-                engine_move = engine.get_best_move(board)
+                engine_move = engine.get_best_move()
 
+            # push the engine move to the board and display board
             engine.made_moves.append(str(engine_move))
-            board.push_san(str(engine_move))
-            evaluations.append(engine.evaluate_board(board))
+            engine.board.push_san(str(engine_move))
+            evaluations.append(engine.evaluate_board(engine.board))
             print(f"\nEngine plays: {engine_move}")
-            print(display(board))
+            print(display(engine.board))
 
+            # get move from user
             user_move = make_move(board)
             if user_move == 'resign':
                 break
-            board.push(user_move)
+            engine.board.push(user_move)
             engine.made_moves.append(str(user_move))
-            evaluations.append(engine.evaluate_board(board))
+            evaluations.append(engine.evaluate_board(engine.board))
     
     else:
         print("Invalid input - enter '1' or '2'")
@@ -75,29 +98,37 @@ def play():
         except KeyboardInterrupt:
             pass
 
+    # check game-result
     if board.result() == "1-0":
         print("White wins!")
     elif board.result() == "0-1":
         print("Black wins!")
+    elif user_move == 'resign':
+        print(f"User has resigned!")
     else:
         print("Game ends in a draw!")
     
+    # display the move-evaluation as evaluated by the engine
     visualize_game(engine.made_moves, evaluations)
 
 
 def display(board):
     """
-    Displays the chess-board in terminal with unicode chess symbols as well as ranks and files.
-    Inspired by Andrew Healey's terminal GUI which in turn is inspired by sunfish.
-    Functions iterates over each square and changes letter value to its unicode representation and
-    prints out the board in the user-terminal.
-    Variables
-    board_str : 127 chr iterable list-representation of the chessboard
+    Displays the chess-board in terminal with unicode chess symbols as well
+    as ranks and files. Inspired by Andrew Healey's terminal GUI.
+    https://healeycodes.com/building-my-own-chess-engine/
+
+    Iterates the board and represents letter by unicode symbols.
+
+    - variables - 
+    board_str : iterable chr-list
     pieces : unicode chess pieces found in constants.py
     """
 
+    # create list representation of chess-board
     board_str = list(str(board))
 
+    # iterate through board-elements
     for i, piece in enumerate(board_str):
         if piece in pieces:
             board_str[i] = pieces[piece]
@@ -105,16 +136,26 @@ def display(board):
     ranks = ["1", "2", "3", "4", "5", "6", "7", "8"]
     uni_board = []
 
+    # add rank and file labels to board
     for rank in "".join(board_str).split("\n"):
         uni_board.append(f"  {ranks.pop()} {rank}")
+
     if board.turn == chess.BLACK:
         uni_board.reverse()
+
     uni_board.append("    a b c d e f g h")
 
     return "\n" + "\n".join(uni_board)
 
 
 def visualize_game(x, y):
+    """
+    Plots a simple line-graph of the board-evaluation.
+
+    - variables -
+    x (list of strings) : list of move-values
+    y (list of ints) : list of board-evaluation at each move
+    """
 
     x.insert(0, 'start')
     y.insert(0, 0)
@@ -122,12 +163,16 @@ def visualize_game(x, y):
     plt.plot(x, y, color='red', marker='o')
     plt.title('Board Evaluation Throughout Game', fontsize=14)
     plt.xlabel('Move', fontsize=12)
+    plt.xticks(rotation = 90)
     plt.ylabel('Evaluation', fontsize=12)
     plt.grid(True)
     plt.show()
 
 
 def make_move(board):
+    """
+    Gets a move from user and returns chess.Move object.
+    """
 
     move = input("('?') for legal moves\nYour move:\n")
 
